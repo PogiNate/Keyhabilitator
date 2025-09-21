913c809529475133755db477d3797190651c9090
Nate Dickson <nate@natedickson.com>
2025-09-21 16:40:48 -0600

    Change white color constant to a darker shade

--- modules/layout_manager.py
+++ modules/layout_manager.py
@@ -11,7 +11,7 @@ class LayoutManager:
         self.color_constants = {
                 "red": (255, 0, 0),
                 "blue": (0, 0, 255),
-                "white": (255, 255, 255),
+                "white": (128, 128, 128),
                 "black": (0, 0, 0),
                 "yellow": (255, 255, 0),
                 "cyan": (0, 255, 255),
